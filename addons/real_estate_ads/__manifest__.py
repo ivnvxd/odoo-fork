3e02146663a1d2136ebbcdaaf8febd48005080f5
Andrey Ivanov <ivnv.xd@gmail.com>
2023-12-12 16:42:36 +0100

    Add real estate ads addon

---
+++ addons/real_estate_ads/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Real Estate Ads",
+    "version": "1.0",
+    "website": "https://ivnv.dev",
+    "author": "Andrey Ivanov",
+    "description": """
+        Real Estate module to show available properties
+    """,
+    "category": "Sales",
+    "depends": [],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
