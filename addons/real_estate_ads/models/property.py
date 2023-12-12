c6d43ccc8d1eaa62ecf993098531117693a64a1e
Andrey Ivanov <ivnv.xd@gmail.com>
2023-12-12 16:43:14 +0100

    Add property model

---
+++ addons/real_estate_ads/models/property.py
@@ -0,0 +1,30 @@
+from odoo import fields, models
+
+
+class Property(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Properties"
+
+    name = fields.Char(string="Name", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available From")
+    expected_price = fields.Float(string="Expected Price")
+    best_offer = fields.Float(string="Best Offer")
+    selling_price = fields.Float(string="Selling Price")
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage", default=False)
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Total Area (sqm)")
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
+        string="Garden Orientation",
+        default="north",
+    )
