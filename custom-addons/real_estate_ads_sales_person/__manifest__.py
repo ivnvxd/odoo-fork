25dde9e73bdaf051c91276e8226b97b79ed47563
Andrey Ivanov <ivnv.xd@gmail.com>
2024-01-13 14:14:18 +0100

    Add Real Estate Ads - Sales Person module

---
+++ custom-addons/real_estate_ads_sales_person/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Real Estate Ads - Sales Person",
+    "version": "1.0",
+    "website": "https://ivnv.dev",
+    "author": "Andrey Ivanov",
+    "description": """
+        Show real estate properties linked to a sales person
+    """,
+    "category": "Sales",
+    "depends": ["real_estate_ads", "base"],
+    "data": [],
+    "installable": True,
+    "license": "LGPL-3",
+}
---
+++ custom-addons/real_estate_ads_sales_person/models/estate_property.py
@@ -0,0 +1,21 @@
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    sales_id = fields.Many2one("res.users", required=True)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            sales_person_property_ids = self.env[self._name].search_count(
+                [("sales_id", "=", vals.get("sales_id"))]
+            )
+            if sales_person_property_ids >= 2:
+                raise ValidationError(
+                    "User already has 2 properties assigned to him/her."
+                )
+
+        return super(EstateProperty, self).create(vals_list)
