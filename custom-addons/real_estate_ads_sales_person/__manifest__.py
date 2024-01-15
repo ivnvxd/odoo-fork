72eaf097e84e8f7ea2f1fd0a1095df7792866444
Andrey Ivanov <ivnv.xd@gmail.com>
2024-01-15 12:55:54 +0100

    Add view inheritance

--- custom-addons/real_estate_ads_sales_person/__manifest__.py
+++ custom-addons/real_estate_ads_sales_person/__manifest__.py
@@ -8,7 +8,9 @@
     """,
     "category": "Sales",
     "depends": ["real_estate_ads", "base"],
-    "data": [],
+    "data": [
+        "views/res_users.xml",
+    ],
     "installable": True,
     "license": "LGPL-3",
 }
--- custom-addons/real_estate_ads_sales_person/models/res_users.py
+++ custom-addons/real_estate_ads_sales_person/models/res_users.py
@@ -5,3 +5,4 @@ class Users(models.Model):
     _inherit = "res.users"
 
     property_ids = fields.One2many("estate.property", "sales_id", string="Properties")
+    # type_id = fields.Many2one("estate.property.type", string="Property Type")
